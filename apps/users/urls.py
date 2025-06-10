1fed5e0efb990c18a9da93850abd87678da94562
Karlos Avakian <39072452+karlosavakian@users.noreply.github.com>
2025-06-10 04:45:34 +0000

    google auth fix

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+
+from .forms import LoginForm
 from .views import auth, review, follow
 from .views import profile as profile_views
-from .forms import LoginForm
-from .views import public, review
-from .views import profile as profile_views
-from django.contrib.auth import views as auth_views 
-from .views import auth
-from .forms import *
 
 urlpatterns = [
     path('register/', auth.register, name='register'),
@@ -20,13 +16,6 @@ urlpatterns = [
     path('favoritos/', profile_views.favorites, name='favoritos'),
     path('mis-reseñas/', review.mis_reseñas, name='mis-reseñas'),
     path('reseña/<slug:slug>/', review.dejar_reseña, name='dejar-reseña'),
-
-    path('profile/', profile_views.profile, name='profile'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('register/', auth.register, name='register'),
-
-    
-    path('reseña/<slug:slug>/', review.dejar_reseña, name='dejar-reseña'),  # Dejar Reseña
   
     path('club/<slug:slug>/editar_reseña/<int:reseña_id>/', review.editar_reseña, name='editar_reseña'),
     path('reseña/<int:reseña_id>/eliminar/', review.eliminar_reseña, name='eliminar-reseña'),
