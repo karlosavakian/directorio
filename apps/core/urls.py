e74639d2760697a24bd5d0c301c90186d6d4036d
Karlos Avakian <39072452+karlosavakian@users.noreply.github.com>
2025-07-11 01:24:09 +0200

    Reemplaza ruta FAQ por Ayuda

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,9 @@
 # apps/core/urls.py
 from django.urls import path 
-from .views import home, faq
+from .views import home, ayuda
 
 urlpatterns = [
     path('', home, name='home'),
-    path('faq/', faq, name='faq'),
+    path('ayuda/', ayuda, name='ayuda'),
 
 ]
