dcb1163055e9c009b4e283a2abfd858ccaa8da5d
Karlos Avakian <39072452+karlosavakian@users.noreply.github.com>
2025-07-11 01:28:37 +0200

    Reemplaza ruta FAQ por Ayuda

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -17,9 +17,9 @@ class YoutubeEmbedTests(SimpleTestCase):
         self.assertIn('&lt;script&gt;alert(1)&lt;/script&gt;', html)
 
 
-class FaqViewTests(SimpleTestCase):
-    def test_faq_url_resolves(self):
-        response = self.client.get('/faq/')
+class AyudaViewTests(SimpleTestCase):
+    def test_ayuda_url_resolves(self):
+        response = self.client.get('/ayuda/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/faq.html')
+        self.assertTemplateUsed(response, 'core/ayuda.html')
 
