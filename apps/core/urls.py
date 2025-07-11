6554cf54c36dde1b6a600747e57bb0d97f020bb5
Karlos Avakian <39072452+karlosavakian@users.noreply.github.com>
2025-07-11 02:54:59 +0200

    Add legal pages and links

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,13 @@
 # apps/core/urls.py
 from django.urls import path 
-from .views import home, ayuda, planes
+from .views import home, ayuda, planes, terminos, privacidad, cookies
 
 urlpatterns = [
     path('', home, name='home'),
     path('ayuda/', ayuda, name='ayuda'),
     path('planes/', planes, name='planes'),
+    path('terminos/', terminos, name='terminos'),
+    path('privacidad/', privacidad, name='privacidad'),
+    path('cookies/', cookies, name='cookies'),
 
 ]
