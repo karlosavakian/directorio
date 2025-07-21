518c2aaf67ef402cee91bd0555b0a00da62cd140
unknown <you@example.com>
2025-07-21 03:43:22 +0200

    booking modal fix

--- apps/clubs/views/__init__.py
+++ apps/clubs/views/__init__.py
@@ -1,7 +1,7 @@
 from .search import search_results
 from .public import club_profile, coach_profile, ajax_reviews
 from .post import post_create, post_update, post_delete, post_reply, post_toggle_like
-from .booking import cancel_booking
+from .booking import cancel_booking, create_booking
 from .dashboard import (
     dashboard,
     club_edit,
