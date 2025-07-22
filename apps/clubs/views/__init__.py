3f453c6bdc9020b7da43fab3587b4ccdff95bd29
Karlos Avakian <39072452+karlosavakian@users.noreply.github.com>
2025-07-22 10:44:53 +0000

    init update

--- apps/clubs/views/__init__.py
+++ apps/clubs/views/__init__.py
@@ -1,7 +1,7 @@
 from .search import search_results
 from .public import club_profile, coach_profile, ajax_reviews
 from .post import post_create, post_update, post_delete, post_reply, post_toggle_like
-from .booking import cancel_booking, create_booking
+from .booking import cancel_booking, create_booking, booking_confirm, booking_cancel_admin
 from .dashboard import (
     dashboard,
     club_edit,
