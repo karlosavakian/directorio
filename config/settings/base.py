74a20ad04476d8c51c27f40394e069da17298d60
Karlos <Karlos@Karloss-MacBook-Pro.local>
2025-08-03 13:15:43 +0200

    badges verification plans ux

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,8 +48,7 @@ if _hosts:
         host.strip() for host in _hosts.split(",") if host.strip()
     ]
 else:
-    ALLOWED_HOSTS = [
-        '25392cc6-0165-4a6c-b600-132f4b818482-00-37q2oz4tepv3s.janeway.replit.dev'
+    ALLOWED_HOSTS = [ 
     ]
 
 _origins = os.environ.get("CSRF_TRUSTED_ORIGINS")
@@ -59,7 +58,6 @@ if _origins:
     ]
 else:
     CSRF_TRUSTED_ORIGINS = [
-        'https://25392cc6-0165-4a6c-b600-132f4b818482-00-37q2oz4tepv3s.janeway.replit.dev'
     ]
 
 INSTALLED_APPS = [
