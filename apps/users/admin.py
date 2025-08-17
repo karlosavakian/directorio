671065b44bf770a902d4ae738f947727c56a28fc
Karlos Avakian <39072452+karlosavakian@users.noreply.github.com>
2025-08-18 00:45:09 +0200

    Allow admin to set user profile type

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 from .models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "location")
+    list_display = ("user", "profile_type", "location")
